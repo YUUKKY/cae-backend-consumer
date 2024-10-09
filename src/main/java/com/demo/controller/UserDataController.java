5a161a66adf1c03d30d041ec95982e9b447ef36a
YUUKKY <43734044+YUUKKY@users.noreply.github.com>
2024-10-09 17:41:28 +0800

    增加接口

--- src/main/java/com/demo/controller/UserDataController.java
+++ src/main/java/com/demo/controller/UserDataController.java
@@ -28,4 +28,30 @@ public class UserDataController {
         }
         return new UserDataVo().setUserName(data.getUserName()).setScanNum(data.getScanNum());
     }
+
+    @CrossOrigin
+    @RequestMapping(value = "/total_scan_num", method = RequestMethod.GET)
+    public Integer getTotalScanNum() {
+        return dataService.getTotalScanNum();
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/consume_cpu", method = RequestMethod.GET)
+    public Integer clientTest() {
+        // 消耗CPU 的计算
+        for (int i = 0; i <= 10; i++) {
+            MyThread thread = new MyThread();
+            thread.start();
+        }
+        return 1;
+    }
+
+    class MyThread extends Thread {
+        @Override
+        public void run() {
+            for (int index = 0; index <= 100000000; index++) {
+                Math.pow(100, 1 / 3);
+            }
+        }
+    }
 }
